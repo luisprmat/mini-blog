2cb3821a4395c2dde59e4bd62344935b6da12e02
Luis Parrado <luisprmat@gmail.com>
2023-02-22 22:43:26 -0500

    Routes with name in Laravel 10

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'welcome');
-Route::view('contact', 'contact');
-Route::view('blog', 'blog');
-Route::view('about', 'about');
+Route::view('/', 'welcome')->name('home');
+Route::view('contact', 'contact')->name('contact');
+Route::view('blog', 'blog')->name('blog');
+Route::view('about', 'about')->name('about');
