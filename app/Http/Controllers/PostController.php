30a276e346137215c9c0ffbeb37424e5e68f0d85
Luis Parrado <luisprmat@gmail.com>
2023-02-25 22:50:30 -0500

    Introduction to Eloquent ORM

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
     public function index()
     {
-        $posts = DB::table('posts')->get();
+        $posts = Post::get();
 
         return view('blog', ['posts' => $posts]);
     }
