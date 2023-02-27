93b03fd053e60509e66fcbe1e68a07173ddeb2f5
Luis Parrado <luisprmat@gmail.com>
2023-02-26 22:22:00 -0500

    Refactor  using Form Request & extract form fields

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -5,24 +5,8 @@
     <h1>Edit form</h1>
 
     <form action="{{ route('posts.update', $post) }}" method="POST">
-        @csrf
         @method('PATCH')
-        <label>
-            Title <br>
-            <input name="title" type="text" value="{{ old('title', $post->title) }}">
-            @error('title')
-                <br>
-                <small style="color: red">{{ $message }}</small>
-            @enderror
-        </label><br>
-        <label>
-            Body <br>
-            <textarea name="body">{{ old('body', $post->body) }}</textarea>
-            @error('body')
-                <br>
-                <small style="color: red">{{ $message }}</small>
-            @enderror
-        </label> <br>
+        @include('posts.form-fields')
         <button type="submit">Send</button>
     </form>
 
